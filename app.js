181449c6aab2a16beb0e3fa14604bf22c77cc481
Harsh Vardhan Pal <hp77335@gmail.com>
2026-04-22 12:37:57 +0530

    first commit for

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+import generateRoutes from "./route/generateRoutes.js";
+import userRoutes from "./route/userRoutes.js";
+
+import "./config/database.js";
+import submitRoutes from "./route/submitRoutes.js";
+import testRoutes from "./route/testRoutes.js";
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  res.send("Hello World!");
+}
+);
+
+app.use("/api",generateRoutes,submitRoutes,testRoutes);
+app.use("/api/user",userRoutes);
+
+const PORT = process.env.PORT || 5010;
+app.listen(PORT, ()=>{
+  console.log("server running")
+})
